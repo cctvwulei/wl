ffadf2826c46da64803e87d789e77b7daa8ac5a2
cctvwulei <1093341707@qq.com>
2017-07-13 16:53:21 +0800

    test31

---
+++ src/main/java/User.java
@@ -0,0 +1,31 @@
+/**
+ * Created by Administrator on 2017/6/26.
+ */
+public class User {
+
+    private String userName;
+
+    private  int userAge;
+
+    public void setUserAge(int userAge) {
+        this.userAge = userAge;
+    }
+
+    public void setUserName(String userName) {
+
+        this.userName = userName;
+    }
+
+    public int getUserAge() {
+
+        return userAge;
+    }
+
+    public String getUserName() {
+
+        return userName;
+    }
+
+    public User() {
+    }
+}
